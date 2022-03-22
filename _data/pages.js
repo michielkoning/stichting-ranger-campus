1d224873bb2c0681c4f00d607d67c9f6ed0a3cdc
Michiel Koning <michiel.koning@valtech.com>
2022-03-22 17:02:14 +0100

    pages

---
+++ _data/pages.js
@@ -0,0 +1,58 @@
+const AssetCache = require("@11ty/eleventy-cache-assets");
+
+/**
+ * Get the articles from WordPress
+ * Uses eleventy-cache-assets to speed up build time
+ */
+async function fetchArticles() {
+  try {
+    return AssetCache(
+      "https://loesje.nl/wp-json/wp/v2/posts?per_page=100&_embed",
+      {
+        duration: "1d",
+        type: "json",
+      }
+    );
+  } catch (error) {
+    console.error(`Error: ${error}`);
+    return [];
+  }
+}
+
+/**
+ * Clean up and convert the API response for our needs
+ */
+async function processPosts(blogposts) {
+  return Promise.all(
+    blogposts.map(async (post) => {
+      // remove HTML-Tags from the excerpt for meta description
+      let metaDescription = post.excerpt.rendered.replace(/(<([^>]+)>)/gi, "");
+      metaDescription = metaDescription.replace("\n", "");
+
+      // Return only the data that is needed for the actual output
+      return await {
+        title: post.title.rendered,
+        date: post.date,
+        formattedDate: new Date(post.date).toLocaleDateString("nl-NL", {
+          weekday: "long",
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        }),
+        rssDate: new Date(post.date).toUTCString(),
+        modifiedDate: post.modified,
+        slug: post.slug,
+        metaDescription: metaDescription,
+        excerpt: post.excerpt.rendered,
+        content: post.content.rendered,
+        categorySlugs: post.msme_categories_slugs,
+      };
+    })
+  );
+}
+
+module.exports = async () => {
+  const blogposts = await fetchArticles();
+  const processedPosts = await processPosts(blogposts);
+  return processedPosts;
+};
--- _data/posts.js
+++ _data/posts.js
@@ -1,9 +1,25 @@
 const fetch = require("node-fetch");
+const Image = require("@11ty/eleventy-img");
 
-module.exports = async function () {
+module.exports = async () => {
   console.log("Fetching data...");
 
-  return fetch("https://api.adler-lingenau.com/wp-json/wp/v2/posts")
-    .then((res) => res.json())
-    .then((json) => json);
+  const response = await fetch(
+    "https://api.adler-lingenau.com/wp-json/wp/v2/posts?_embed"
+  );
+  const posts = await response.json();
+
+  posts.forEach(async (post) => {
+    if (post._embedded["wp:featuredmedia"]) {
+      if (post._embedded["wp:featuredmedia"]["0"].source_url) {
+        const url = post._embedded["wp:featuredmedia"]["0"].source_url;
+        await Image(url, {
+          widths: [300],
+          formats: ["webp", "jpeg", "avif"],
+        });
+      }
+    }
+  });
+
+  return posts;
 };
